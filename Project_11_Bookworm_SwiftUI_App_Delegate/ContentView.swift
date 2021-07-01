ca7b1de7cfda1a4c8335be14b600e66429d70958
Baris <karalarbaris@gmail.com>
2021-07-01 10:39:35 +0200

    Initial Commit

---
+++ Project_11_Bookworm_SwiftUI_App_Delegate/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Project_11_Bookworm_SwiftUI_App_Delegate
+//
+//  Created by Baris Karalar on 1.07.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
